5bfd88b8f0c327344f6010bf8ad0042896a79730
18010105 <18010105@PRCNMG1L0311>
2018-01-23 17:11:18 +0800

    添加项目文件。

---
+++ ChatWebApp/ChatHub.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+using Microsoft.AspNet.SignalR;
+using Microsoft.AspNet.SignalR.Hubs;
+
+namespace ChatWebApp
+{
+    [HubName("ChatHub")]
+    public class ChatHub : Hub
+    {
+        public void Hello()
+        {
+            Clients.All.hello();
+        }
+
+
+        public override Task OnConnected()
+        {
+            Clients.All.hello(this.Context.ConnectionId);
+            Clients.All.hello("connected");
+            return base.OnConnected();
+        }
+
+        public override Task OnReconnected()
+        {
+            Clients.All.hello(this.Context.ConnectionId);
+            Clients.All.hello("connected");
+            return base.OnReconnected();
+        }
+    }
+}
